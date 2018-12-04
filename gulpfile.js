d99c94644bab8ebba286f453d1460cd829da9e4f
Brainsoap <vjaszai@brainsum.com>
2018-12-04 13:32:26 +0100

    Initial framework established.

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+var gulp = require('gulp');
+var styleguide = require('sc5-styleguide');
+var sass = require('gulp-sass');
+var tempPath = 'tmp';
+var outputPath = 'docs';
+
+gulp.task('styleguide:generate:dev', function() {
+  return gulp.src('styles-guide/**/*.scss')
+    .pipe(styleguide.generate({
+        title: 'Tieto Styleguide',
+        server: true,
+        rootPath: tempPath,
+        overviewPath: 'README.md'
+      }))
+    .pipe(gulp.dest(tempPath));
+});
+
+gulp.task('styleguide:applystyles:dev', function() {
+    return gulp.src('styles-guide/main.scss')
+      .pipe(sass({
+        errLogToConsole: true
+      }))
+      .pipe(styleguide.applyStyles())
+      .pipe(gulp.dest(tempPath));
+  });
+
+gulp.task('styleguide:generate:build', function() {
+  return gulp.src('styles-guide/**/*.scss')
+    .pipe(styleguide.generate({
+        title: 'Tieto Styleguide',
+        server: false,
+        rootPath: outputPath,
+        appRoot: '.',
+        overviewPath: 'README.md'
+      }))
+    .pipe(gulp.dest(outputPath));
+});
+
+gulp.task('styleguide:applystyles:build', function() {
+  return gulp.src('styles-guide/main.scss')
+    .pipe(sass({
+      errLogToConsole: true
+    }))
+    .pipe(styleguide.applyStyles())
+    .pipe(gulp.dest(outputPath));
+});
+
+gulp.task('styleguide:devtask', ['styleguide:generate:dev', 'styleguide:applystyles:dev']);
+gulp.task('styleguide:dev', ['styleguide:devtask'], function() {
+  // Start watching changes and update styleguide whenever changes are detected
+  // Styleguide automatically detects existing server instance
+  gulp.watch(['styles-guide/**/*.scss', 'styles-library/**/*.scss'], ['styleguide:devtask']);
+});
+
+gulp.task('styleguide:build', ['styleguide:generate:build', 'styleguide:applystyles:build']);
